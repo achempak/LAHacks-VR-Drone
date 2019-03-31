31fce68715b6fe3ee3d14b543eebf9059b52d608
Hoang Nguyen <hnguyen247@gmail.com>
2019-03-31 08:33:32 -0700

    Add files via upload

--- Networking/Client.cpp
+++ Networking/Client.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	string ipAddress = "192.168.4.16";
+	string ipAddress = "192.168.4.1";
 	int port = 8080;
 	//Initialize Winsock
 	WSAData data;
@@ -44,21 +44,34 @@ int main()
 	//Do-while loop to send and receive data
 	char buf[60000];
 	string userInput;
-
+	
 	while (true) {
+		//Sleep(30);
 		int bytesReceived = recv(sock, buf, 60000, 0);
 		if (bytesReceived > 0) {
 			FILE* lol;
-			errno_t err = fopen_s(&lol, "C:/Users/HNguy/Downloads/newServer.jpg", "wb+");
+			errno_t err = fopen_s(&lol, "../newPicture.jpg", "wb+");
 			//fseek(lol, 0, SEEK_SET);
 			fwrite(buf, 1, bytesReceived, lol);
-			cout << bytesReceived << endl;
-			fclose(lol);
+			cout <<"outer "<< bytesReceived << endl;
+			//memset(buf, 0, 60000);
+			//fclose(lol);
+			//while((bytesReceived = recv(sock, buf, 60000, 0)) > 0) {
+			//	FILE* lol2;
+			//	errno_t err2 = fopen_s(&lol2, "C:/Users/HNguy/Downloads/newServer.jpg", "ab+");
+			//	fwrite(buf, 1, bytesReceived, lol);
+			//	cout <<"inner " << bytesReceived << endl;
+//				fclose(lol2);
+		//	}
 //			fflush()
 			//break;
 			//memset(buf, 0, 60000);
+			fclose(lol);
+			
 		}
 	}
+
+//	fclose(lol);
 	closesocket(sock);
 	WSACleanup();
 }
